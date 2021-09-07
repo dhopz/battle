8c04df28231a783b9c98c09e26611bbb5b75c14e
David Houpapa <david_houpapa@hotmail.com>
2021-09-07 16:41:41 +0100

    viewing hit points

---
+++ app.rb
@@ -0,0 +1,33 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  enable :sessions
+  
+#   get '/' do
+#     'Testing infrastructure working!'
+#   end
+
+  get '/' do
+    erb :index
+  end  
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
