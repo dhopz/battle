e349d0f3fccc0430f6ffb36386414cf1de186461
David Houpapa <david_houpapa@hotmail.com>
2021-09-08 11:46:27 +0100

    Implementing Hit Points

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/player'
 
 class Battle < Sinatra::Base
   configure :development do
@@ -29,8 +30,9 @@ class Battle < Sinatra::Base
   end
   
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
     erb :attack
   end
 
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,13 @@ feature 'Attacking' do
       click_link 'Attack'
       expect(page).to have_content 'Charlotte attacked Mittens'
     end
+
+    scenario 'reduce Player 2 HP by 10' do
+      sign_in_and_play
+      click_link 'Attack'
+      #click_link 'OK'
+      expect(page).not_to have_content 'Mittens: 60HP'
+      expect(page).to have_content 'Mittens: 50HP'
+    end
+
   end
